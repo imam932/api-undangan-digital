dd4625e5f2a6abb63a2d856e69c24d32e1d84921
nawawi932@gmail.com <imam@imam.nawawi>
2025-10-16 22:44:33 -0400

    update time

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Message extends Model
 {
@@ -13,4 +14,16 @@ class Message extends Model
         'name',
         'message',
     ];
+
+    /**
+     * Accessor: format created_at to "10 Oct 25 14:30 wib" (Asia/Jakarta).
+     * This affects JSON serialization while keeping DB ordering intact.
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        // Ensure we parse and convert timezone to Asia/Jakarta, then format.
+        return Carbon::parse($value)
+            ->timezone('Asia/Jakarta')
+            ->format('d M y H:i') . ' WIB';
+    }
 }
